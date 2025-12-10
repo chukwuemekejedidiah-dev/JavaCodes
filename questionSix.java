e0c298eba90d56cad9b8d5abb03864a1d33329f3
SaitamaPunch <chukwuemekejedidiah@gmail.com>
2025-12-10 13:33:08 +0100

    Add files via upload

---
+++ questionSix.java
@@ -0,0 +1,48 @@
+// Sum of Even Numbers between 1 and 50.
+//public class questionSix {
+//    public static void main(String[] args) {
+//        int i = 1, sum = 0;
+//
+//        while (i <= 50) {
+//            if (i % 2 == 0) {
+//                sum += i;
+//            }
+//            i++;
+//        }
+//
+//        System.out.println("Sum of even numbers from 1 to 50: " + sum);
+//    }
+//}
+
+
+
+
+// Largest 10 Numbers.
+//import java.util.Scanner;
+//public class questionSix {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int count = 1, number, largest;
+//
+//        System.out.print("Enter number 1: ");
+//        number = input.nextInt();
+//        largest = number;
+//
+//        while (count < 10) {
+//            System.out.print("Enter number " + (count + 1) + ": ");
+//            number = input.nextInt();
+//
+//            if (number > largest) {
+//                largest = number;
+//            }
+//
+//            count++;
+//        }
+//
+//        System.out.println("The largest number is: " + largest);
+//        input.close();
+//    }
+//}
+
+
+
